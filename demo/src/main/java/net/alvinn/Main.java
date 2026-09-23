0dc10ba62eb1c5388f79346b52ec98daebd70ba2
student <student@itismeucci.com>
2026-09-23 08:37:10 +0200

    move to jdk21

--- demo/src/main/java/net/alvinn/Main.java
+++ demo/src/main/java/net/alvinn/Main.java
@@ -2,6 +2,6 @@ package net.alvinn;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world! UwU");
     }
 }
